65ae4ada0be073f8111ffe693b73752d0a1e7a2a
Jordan Than <jordanthan7@gmail.com>
2024-11-13 09:42:27 -0500

    jordan

--- models/properties.js
+++ models/properties.js
@@ -3,12 +3,19 @@ const propertiesDB = require('../config/propertiesDB'); // Use propertiesDB conn
 
 const propertySchema = new Schema({
   property_id: { type: String, required: true, unique: true },
-  address: { type: String, required: true },
   owner_id: { type: Schema.Types.ObjectId, ref: 'Owner' },
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  province: { type: String, required: true },
+  country: { type: String, required: true },
+  propertyType: { type: String, required: true },
+  sizeSqft: { type: Number, required: true },
   value: { type: Number, required: true },
-  status: { type: String, default: 'Available' },
-  description: { type: String },
-  listing_date: { type: Date, default: Date.now },
+  purchase_price: { type: Number, required: true },  
+  purchase_date: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = propertiesDB.model('Property', propertySchema);
