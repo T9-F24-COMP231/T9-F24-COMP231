79a0ab5c530d9ddd4da4755e3727beac6daace93
Dmitrii Mitin <mda_200418@mail.ru>
2024-11-14 21:07:14 -0500

    Creating Header and Footer components with styles for each user roles

--- propertyClear/client/src/components/Footer.jsx
+++ propertyClear/client/src/components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "../assets/styles/header_footer.css";
 
 const Footer = () => {
     return(
-        <div>
-            <h1>footer</h1>
+        <div className="footer_content">
+            <div>
+                <p>&copy; 2024 PropertyClear</p>
+            </div>
+
+            <div>
+                <p>All rights reserved.</p>
+            </div>
         </div>
     )
 }
