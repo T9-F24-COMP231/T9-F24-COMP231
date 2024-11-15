432fba4536184d7bdbb1d9c0dbba34b990e2b36e
Dmitrii Mitin <mda_200418@mail.ru>
2024-11-15 01:11:53 -0500

    Connecting database with the server

--- propertyClear/server/routes/user.routes.js
+++ propertyClear/server/routes/user.routes.js
@@ -10,18 +10,10 @@ import authCtrl from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-// Public Routes
-router.get('/user/data', authCtrl.requireSignin, getUserData);
-
-// Administrator Routes
+router.get('/data', authCtrl.requireSignin, getUserData);
 router.get('/admin/users', authCtrl.requireSignin, authCtrl.authorizeRoles(['Administrator']), getAllUsers);
-
-// Mortgage Broker Routes
 router.get('/broker/mortgage/:propertyId', authCtrl.requireSignin, authCtrl.authorizeRoles(['MortgageBroker']), getMortgageData);
-
-// Real Estate Agent Routes
 router.get('/agent/listings', authCtrl.requireSignin, authCtrl.authorizeRoles(['RealEstateAgent']), getListings);
 
-// Other role-specific routes can be added similarly
-
 export default router;
+
