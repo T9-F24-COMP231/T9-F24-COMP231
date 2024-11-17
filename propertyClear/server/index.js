6d5c3e77d4fc15c0bf9bcfa61bfef4141db7ee13
rthchldshynee <120283958+rthchldshynee@users.noreply.github.com>
2024-11-17 13:40:49 -0500

    got db example working

--- propertyClear/server/index.js
+++ propertyClear/server/index.js
@@ -1,67 +1,5 @@
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import bodyParser from 'body-parser';
-// import cookieParser from 'cookie-parser';
 import config from './config/config.js';
-// import userRoutes from './routes/user.routes.js';
-// import propertyRoutes from './routes/property.routes.js';
-// import lienRoutes from './routes/lien.routes.js';
-
-// const app = express();
-// const PORT = process.env.PORT || 5001;
-
-// // Middleware
-// app.use(bodyParser.json());
-// app.use(cookieParser());
-
-// // MongoDB Connections
-// const connectDB = async () => {
-//   try {
-//     console.log(config.propertiesDbUri);
-//     await mongoose.connect(config.propertiesDbUri, { useNewUrlParser: true, useUnifiedTopology: true });
-//     console.log('Connected to Properties and User database');
-//   } catch (err) {
-//     console.error('Properties DB connection error:', err);
-//   }
-// };
-// connectDB();
-
-// // Routes
-// app.use('/api/users', userRoutes);
-// app.use('/api/liens', lienRoutes);
-// app.use('/api/properties', propertyRoutes);
-
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// app.get('/notification', (req, res) => {
-//   res.send('Im going to send an email');
-// });
-
-
-// // import mongoose from 'mongoose';
-
-// // const connectDatabases = async () => {
-// //   try {
-// //     // Connect to Properties Database
-// //     await mongoose.connect(config.propertiesDbUri, {
-// //       useNewUrlParser: true,
-// //       useUnifiedTopology: true,
-// //     });
-// //     console.log('Connected to Properties and User database');
-
-// //     // Create a new Mongoose connection for the Liens Database
-// //     const liensConnection = await mongoose.createConnection(config.liensDbUri, {
-// //       useNewUrlParser: true,
-// //       useUnifiedTopology: true,
-// //     });
-// //     console.log('Connected to Liens database');
-
-// //   } catch (err) {
-// //     console.error('Database connection error:', err);
-// //   }
-// // };
-
-// // connectDatabases();
+import { getPool } from './config/db.js';
 
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -78,6 +16,13 @@ app.use(cookieParser());
 // Basic test route
 app.get('/', (req, res) => {
   res.json({ message: 'Server is running!' });
+});
+
+app.get('/users', async (req, res) => {
+  const pool = await getPool();
+  let users = await pool.query(`SELECT * FROM "User"`);
+  console.log(users);
+  res.json(users);
 });
 
 app.get('/notification', (req, res) => {
