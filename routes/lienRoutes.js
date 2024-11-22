aebc4d71098ae53c19ca7f3af1757b6abe2995e5
Jordan <169177147+NeoHt1@users.noreply.github.com>
2024-11-21 19:07:28 -0500

    Delete routes directory

--- routes/lienRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllLiens, getLienById } = require('../controllers/lienController');
-
-router.get('/', getAllLiens);
-router.get('/:id', getLienById);
-
-module.exports = router;
--- routes/propertyRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllProperties, getPropertyById } = require('../controllers/propertyController');
-
-router.get('/', getAllProperties);
-router.get('/:id', getPropertyById);
-
-module.exports = router;
